ecde8ef060b064b5ae21136b82a9adeeb44ae782
Abhinash Khatiwada <me@abhinash.net>
2023-09-23 12:14:37 +0545

    feat: create migrations

---
+++ apps/jobs/prisma/migrations/20230923062618_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "jobs" (
+    "id" SERIAL NOT NULL,
+    "userId" BIGINT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "jobs_pkey" PRIMARY KEY ("id")
+);
