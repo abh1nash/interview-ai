8286aa967b35651cf22471cb22ce6153b7683b10
Abhinash Khatiwada <me@abhinash.net>
2023-09-24 14:15:05 +0545

    feat: create frontend app

---
+++ apps/frontend/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="min-h-screen bg-gradient-to-b from-blue-100 to-white overflow-x-hidden"
+  >
+    <div class="mx-auto max-w-3xl">
+      <slot></slot>
+    </div>
+  </div>
+</template>
---
+++ apps/frontend/pages/index.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="text-4xl font-bold font-display">Jello</div>
+</template>
